f09359b9344451284352443bb7c698e512cdb4b2
Davide Rossi <davidelrossi>
2022-12-06 23:52:01 +0100

    202212065

---
+++ dags/past_gameweeks.py
@@ -0,0 +1,6 @@
+"""
+This python module will obtain data from section history
+of the element-summary/{player_id}/ endpoint of the premier league API.
+This section contains a list of player’s previous fixtures and its match stats..
+
+"""
