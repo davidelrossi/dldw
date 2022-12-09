2240f84945f6ab06f69bd5479235402f62f05024
Davide Rossi <davidelrossi>
2022-12-09 15:31:19 +0100

    20221209

--- dags/tea_dag.py
+++ dags/tea_dag.py
@@ -35,38 +35,11 @@ def get_data():
         schema='fpl_api'
     )
 
-    # SQL statements
-
-
-    drop_table = "DROP TABLE IF EXISTS Teams;"
-
-    create_table ="CREATE TABLE IF NOT EXISTS Teams (code INT, draw INT, form VARCHAR(255),\
-            id INT, loss INT, name VARCHAR(255),\
-            played INT, points INT, position INT, short_name VARCHAR(255), strength INT, team_division VARCHAR(255),\
-            unavailable VARCHAR(255), win INT, strength_overall_home INT, strength_overall_away INT,\
-            strength_attack_home INT, strength_attack_away INT, strength_defence_home INT, strength_defence_away INT,\
-            pulse_id INT);"
-
-    # Generate the column names for the INSERT statement
-    cols = ', '.join(teams_df.columns)
-
-    # Generate placeholders for the values
-    placeholders = ', '.join(['%s'] * len(teams_df.columns))
-
-    insert = "INSERT INTO Teams ({}) VALUES ({})".format(cols, placeholders)
-
-    # Connect to data lake
-    pg_conn = pg_hook.get_conn()
-    cursor = pg_conn.cursor()
-
-    # Execute SQL statements
-    cursor.execute(drop_table)
-    cursor.execute(create_table)
-
-    # Insert data into Data Lake
-    cursor.executemany(insert, teams_df)
-    pg_conn.commit()
-    print("The Dataframe is Inserted")
+    hook.pandas_to_postgres(
+        dataframe=teams_df,
+        table_name="teams",
+        if_exists="replace"
+    )
 
 
 # Create DAG
