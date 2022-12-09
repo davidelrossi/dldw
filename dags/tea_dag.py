f86364240d3bfe73899a3dec9c39e41f5f9c0608
Davide Rossi <davidelrossi>
2022-12-09 15:46:23 +0100

    20221209

--- dags/tea_dag.py
+++ dags/tea_dag.py
@@ -35,11 +35,29 @@ def get_data():
         schema='fpl_api'
     )
 
-    pg_hook.pandas_to_postgres(
-        dataframe=teams_df,
-        table_name="teams",
-        if_exists="replace"
-    )
+    drop_table = "DROP TABLE IF EXISTS Teams;"
+
+    create_table ="CREATE TABLE IF NOT EXISTS Teams (code INT, draw INT, form VARCHAR(255),\
+            id INT, loss INT, name VARCHAR(255),\
+            played INT, points INT, position INT, short_name VARCHAR(255), strength INT, team_division VARCHAR(255),\
+            unavailable VARCHAR(255), win INT, strength_overall_home INT, strength_overall_away INT,\
+            strength_attack_home INT, strength_attack_away INT, strength_defence_home INT, strength_defence_away INT,\
+            pulse_id INT);"
+
+    # Create a list of tuples representing the rows in the dataframe
+    rows = [tuple(x) for x in teams_df.values]
+
+    # Connect to data lake
+    pg_conn = pg_hook.get_conn()
+    cursor = pg_conn.cursor()
+
+    # Execute SQL statements
+    cursor.execute(drop_table)
+    cursor.execute(create_table)
+    # Insert the rows into the database
+    pg_hook.insert_rows(table="Teams", rows=rows)
+
+    pg_conn.commit()
 
 
 # Create DAG
